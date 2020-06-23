31cdf215f8db30d33d42a321ab6f6bb37622d87a
brody sullivan <brodysullivan0@gmail.com>
2020-06-23 15:24:04 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -3,7 +3,7 @@ class User
   attr_accessor :first_name, :last_name
   
   def initialize(first_name, last_name)
-    @first_name = frist_name
-    @last_name = last_name
+    @first_name = n
+    @last_name = n
   end
 end
